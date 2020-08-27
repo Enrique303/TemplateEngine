1e9acd6d2a65bad446f9cf4d970e8cc6bc8a0723
Enrique Lopez <elop22@gmail.com>
2020-08-26 18:14:06 -0600

    deleted and reinstalled package json, worked on appjs

--- app.js
+++ app.js
@@ -10,10 +10,118 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
+const teamMembers = [];
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+function askManager(){
+  inquirer.prompt(
+    [
+      {
+        type: "input",
+        name: "name",
+        message: "what is the managers name?",
+        validate: function(answer) {
+          if(answer !== "") {
+            return true;
+          }
+          return "no empty spaces"
+        }
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "what is your email address?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message:"?"
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "what is the office number?"
+      }
+    ]
+  ).then(function(answers){
+    const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+    teamMembers.push(manager)
+  })
+}
 
+function askIntern(){
+  inquirer.prompt(
+    [
+      {
+        type: "input",
+        name: "name",
+        message: "",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "what is your email address?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message:"?"
+      },
+      {
+        type: "input",
+        name: "school",
+        message: ""
+      }
+    ]
+  ).then(function(answers){
+    const intern = new Intern(answers.name, answers.id, answers.email, answers.officeNumber);
+    teamMembers.push(intern)
+  })
+}
+
+function askEngineer(){
+  inquirer.prompt(
+    [
+      {
+        type: "input",
+        name: "name",
+        message: "",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "what is your email address?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message:"?"
+      },
+      {
+        type: "input",
+        name: "github",
+        message: ""
+      }
+    ]
+  ).then(function(answers){
+    const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+    teamMembers.push(engineer)
+  })
+}
+
+function addEmployees() {
+  inquirer.prompt([{
+    type: "list",
+    name: "switch",
+    choices: ["engineer","intern","manager", "done"],
+    message: "add employees?"
+  }]).then(function(answer){
+    switch(answer.switch){
+      case "engineer":
+        askEngineer()
+    }
+  })
+}
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
